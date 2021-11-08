eadae3c140ac817995cbf0ba52e591c5100ee5a5
Jaejeong98 <jaejeong98@naver.com>
2021-11-09 04:45:19 +0900

    전통음식(술)리스트 받아오기

--- prototype/js/test.js
+++ prototype/js/test.js
@@ -36,3 +36,25 @@ function run(gu){
   }
   
 }
+
+function load_d(){
+  var jsonData = JSON.parse(JSON.stringify(d));
+        var arr=[];
+        for (i in jsonData.Sheet1){
+            console.log(jsonData.Sheet1[i].식품명+":"+jsonData.Sheet1[i].조리법)
+            arr.push(jsonData.Sheet1[i].식품명)
+            arr.push(jsonData.Sheet1[i].출전문헌+" | "+jsonData.Sheet1[i].간행년도)
+            arr.push(jsonData.Sheet1[i].조리법)
+            arr.push("---------------------------------------------------------------------------------------------------------------------------------------")
+        }
+
+    const element=document.getElementById('식품명');
+    if(arr.length>0){
+    let s=""
+
+    for (i in arr){
+      s+=arr[i]+"<br><br>"
+    }
+    element.innerHTML=s;
+  }
+}
